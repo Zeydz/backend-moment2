d1166f078e4c726316ffb2bf1bf9de8af397bf24
Zeydz <39154045+Zeydz@users.noreply.github.com>
2024-04-08 10:26:40 +0200

    Börjat på "mall"

--- server.js
+++ server.js
@@ -5,10 +5,28 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
+app.get('/api', (req, res) => {
+    res.json({message: "Welcome to my API"});
+});
+
+app.get('api/users', (req, res) => {
+    res.json({message: "Get users"});
+});
+
+app.post("/api/users/:id", (req, res) => {
+     res.json({message: "Users added"})
+})
+
+app.put("/api/users/:id", (req, res) => {
+    res.json({message: "Users updated: " + req.params.id})
+})
+
+app.delete("/api/users/:id", (req, res) => {
+    res.json({message: "Users deleted: " + req.params.id})
+})
+
+
 app.listen (port, () => {
     console.log('Server is running on port ' + port);
 })
 
-app.get('/api', (req, res) => {
-    res.json({message: "Welcome to my API"});
-});
