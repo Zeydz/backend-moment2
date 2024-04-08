6455f159c9504ee23a831baf45fbccf8556ea7c4
Jocke <joakim03moller@gmail.com>
2024-04-08 23:10:27 +0200

    Fixade miss med middleware

--- server.js
+++ server.js
@@ -2,19 +2,19 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
-const { Client} = require('pg');
+const { Client } = require('pg');
 require("dotenv").config();
 
 app.use(cors());
+app.use(express.json());
 
 /* Skapa PostgreSQL klient */
-
-const client = new Client ({
-    user: process.env.DB_USER,
+const client = new Client({
     host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     ssl: {
         rejectUnauthorized: false,
     },
@@ -27,14 +27,14 @@ client.connect()
 
 /* Välkommen route */
 app.get('/api', (req, res) => {
-    res.json({message: "Welcome to my API for managing work experiences. Created by Joakim"});
+    res.json({ message: "Welcome to my API for managing work experiences. Created by Joakim" });
 });
 
 /* Route för att hämta alla arbetserfarenheter */
 app.get('/api/work-experiences', (req, res) => {
     client.query("SELECT * FROM workexperiences", (err, result) => {
         if (err) {
-            res.status(500).json({message: "No work experiences found."});
+            res.status(500).json({ message: "No work experiences found." });
         } else {
             res.json(result);
         }
@@ -47,10 +47,11 @@ app.get('/api/work-experiences/:id', (req, res) => {
 
     client.query("SELECT * FROM workexperiences WHERE id = $1", [id], (err, result) => {
         if (err) {
-            res.status(500).json({message: "Error retrieving work experience."});
+            res.status(500).json({ message: "Error retrieving work experience." });
         } else {
+            /* Kontrollerar ifall där finns data i databasen */
             if (result.rows.length === 0) {
-                res.status(404).json({message: "No work experiences found for that specific ID."});
+                res.status(404).json({ message: "No work experiences found for that specific ID." });
             } else {
                 res.json(result.rows[0]);
             }
@@ -60,22 +61,37 @@ app.get('/api/work-experiences/:id', (req, res) => {
 
 /* Route för att lägga till en arbetserfarenhet */
 app.post("/api/work-experiences", (req, res) => {
-     res.json({message: "Work experience added"})
-})
+    const { companyname, jobtitle, location, startdate, enddate, description } = req.body;
+
+    /* Validera att alla fält är ifyllda */
+    if (!companyname || !jobtitle || !location || !startdate || !enddate || !description) {
+        return res.status(400).json({ message: "Please fill in all the required fields." })
+    }
+
+    /* Uppdaterar värde i databas */
+    client.query("INSERT INTO workexperiences (companyname, jobtitle, location, startdate, enddate, description) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+        [companyname, jobtitle, location, startdate, enddate, description], (err, result) => {
+            if (err) {
+                return res.status(500).json({ message: "Error adding work experience" });
+            }
+            res.status(201).json(result.rows[0]);
+        }
+    );
+});
 
 /* Route för att ändra/uppdatera en arbetserfarenhet */
 app.put("/api/work-experiences/:id", (req, res) => {
     const id = req.params.id;
-    res.json({message: "Work experience updated with id: " + id})
+    res.json({ message: "Work experience updated with id: " + id })
 })
 
 /* Route för att ta bort arbetserfarenhet */
 app.delete("/api/users/:id", (req, res) => {
-    res.json({message: "Users deleted: " + req.params.id})
+    res.json({ message: "Users deleted: " + req.params.id })
 })
 
 
-app.listen (port, () => {
+app.listen(port, () => {
     console.log('Server is running on port ' + port);
 })
 
